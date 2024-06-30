1b2abd624264b204ab97de881d808ec96033c0a8
prik73 <prinovac@gmail.com>
2024-06-30 18:06:01 +0530

    just some sphere particles flying around, following doc, plus vid, understanding very few of it, still

---
+++ script.js
@@ -0,0 +1,126 @@
+const canvas = document.getElementById('c1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; 
+
+console.log(ctx)
+
+// ctx.fillStyle = 'white';
+// ctx.strokeStyle = 'white'
+// ctx.lineWidth = 30;
+// ctx.lineCap = 'round'
+
+// ctx.beginPath();
+// ctx.moveTo(100, 200); // line ki starting
+// ctx.lineTo(300, 500); // line ki ending
+
+// ctx.stroke()
+
+let particleArray;
+
+let mouse ={
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.height/80)
+}
+
+window.addEventListener('mousemove',
+    function(event){
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+)
+
+
+class Particle{
+    constructor(x, y, directionX, directionY, size, color){
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+
+    //funciton to make individual particle
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#8C5523';
+        ctx.fill();
+    }
+
+
+    // to check particle, mouse  ki position
+    update(){
+        if(this.x > canvas.width || this.x < 0){
+            this.directionX = - this.directionX;
+        }
+
+        if(this.y > canvas.width || this.y < 0){
+            this.directionY = - this.directionY;
+        }
+
+        
+        // checking colllosion of particles
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx* dx + dy* dy);
+        if(distance < mouse.radius + this.size){
+            
+                //for x
+                if(mouse.x < this.x && this.x < canvas.width - this.size * 10){
+                    this.x += 10;
+                }
+                if( mouse.x > this.x && this.x > this.size * 10){
+                    this.x -= 10;
+                }
+
+                //for y
+                if( mouse.y < this.y && this.x < canvas.height - this.size * 10){
+                    this.y   += 10;
+                }
+                if( mouse.y > this.x && this.y > this.size * 10){
+                    this.y -= 10;
+                }
+            }
+
+            //moving particles
+        
+            this.x += this.directionX;
+            this.y += this.directionY;
+            this.draw();
+
+
+        }
+    }
+
+
+    function init(){
+        particleArray = [];
+        let totalParticles = (canvas.height * canvas.width) / 9000;
+
+        for(let i = 0 ; i < totalParticles; i++){
+            let size = (Math.random() * 5) + 1;
+            let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+            let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+            let directionX = (Math.random() * 5) - 2.5;
+            let directionY = (Math.random() * 5) - 2.5;
+            let color = '#8C5523';
+
+            particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+        }
+    }
+
+    function animate(){
+        requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+        for(let i = 0; i < particleArray.length; i++){
+            particleArray[i].update();
+        }
+    }
+
+
+    init();
+    animate();
